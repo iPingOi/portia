ccef1960a0048b817944bd6fa92cbad39f1b2240
Gabriel Carneiro <gabriel.alvikar@gmail.com>
2023-05-29 13:08:09 -0300

    fix: loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,9 +4,9 @@ import { Cpu } from '@phosphor-icons/react'
 
 export default function Loading (): JSX.Element {
   return (
-    <div className="flex justify-center items-center flex-col h-[calc(100vh_-_93px)]">
+    <main className="flex justify-center items-center flex-col h-[calc(100vh_-_93px)]">
       <Cpu size={64} weight="duotone" className="text-[#8257e5] animate-pulse mb-3" />
       <span className="font-bold">Loading content, please wait!</span>
-    </div>
+    </main>
   )
 }
